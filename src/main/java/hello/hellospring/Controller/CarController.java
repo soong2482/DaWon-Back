07756625028ab14067b851ae93e247ae3b4e24df
soong2482 <soong2482@naver.com>
2024-01-03 11:03:19 +0900

    back

--- src/main/java/hello/hellospring/Controller/CarController.java
+++ src/main/java/hello/hellospring/Controller/CarController.java
@@ -7,6 +7,7 @@ import hello.hellospring.Service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class CarController {
     }
 
     @GetMapping("/all")
+    @ResponseBody
     public List<Car> getAllCar(){
         return carService.getList();
     }
